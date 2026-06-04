b71c4a8b1a6f5180f6951c2a654134d84ba6fc2e
itsnotvii <islandcity57@gmail.com>
2026-06-04 00:52:55 -0700

    added error handling logic

--- app/listings/[id]/page.tsx
+++ app/listings/[id]/page.tsx
@@ -4,5 +4,18 @@ import { listings } from "../../data";
 
 export default async function ListingPage({
     params,
-    
-})
+} : {
+    params: Promise<{ id: string }>;
+}) {
+    const { id } = await params; 
+    const listing = listings.find((l) => l.id === Number(id));
+
+    if (!listing) {
+        return (
+            <div className="min-h-screen flex items-center justify-center">
+                <p className="text-gray-500">Listing not found.</p>
+            </div>
+        );
+    }
+}
+
