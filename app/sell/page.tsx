d936292054bf9b582e31467496a7658b31bcdb92
itsnotvii <islandcity57@gmail.com>
2026-07-07 22:55:33 -0700

    additional bug fixes and api errors

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -208,7 +208,7 @@ export default function SellPage() {
               type="number"
               placeholder="0"
               className="w-full border border-gray-300 rounded-xl px-4 py-2 text-sm focus:outline-none focus:border-red-400"
-            />
+            /> 
           </div>
 
           {/* Category */}
